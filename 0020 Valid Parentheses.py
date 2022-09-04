ebc3581b8a9b5fdbb204cc1a36c364f4a82d0c80
Plant Power <cainveg@gmail.com>
2022-09-03 21:24:55 -0700

    last

--- 0020 Valid Parentheses.py
+++ 0020 Valid Parentheses.py
@@ -9,22 +9,22 @@ def isValid(s):
             s = s.replace(x, y)
         print(s)
         i = 0
+        rel = True
         while i < n-1:
             if int(s[i]) != (int(s[i+1]) - 3):
                 rel = False
                 break
             i += 2
-            rel = True
         if rel != True:
+            rel = True
             i = 0
             while i < int(n/2):
                 if int(s[i]) != (int(s[n -1 -i]) - 3):
                     rel = False
                     break
-                i += 1
-                rel = True
+                i += 1                
     return print(rel)
 
-s = "{{{{[]}}}}"
+s = "{["
 
 isValid(s)
