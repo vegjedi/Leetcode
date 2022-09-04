d4f42d27dd284903bdabfb79df064653a34f6981
Plant Power <cainveg@gmail.com>
2022-09-03 21:17:56 -0700

    final

--- 0020 Valid Parentheses.py
+++ 0020 Valid Parentheses.py
@@ -1,5 +1,5 @@
 def isValid(s):
-    n = len(s) 
+    n = len(s)
     if n%2 == 1:
         rel = False
     else:
@@ -7,22 +7,24 @@ def isValid(s):
         l2 =['1','2','3','4','5','6']
         for x,y in zip(l1, l2):
             s = s.replace(x, y)
+        print(s)
         i = 0
         while i < n-1:
             if int(s[i]) != (int(s[i+1]) - 3):
                 rel = False
                 break
-            i += 1
-        i = 0
-        while i < int(n/2):
-            if int(s[i]) != (int(s[n -i]) - 3):
-                rel = False
-                break
-            i += 1
-        else: 
+            i += 2
             rel = True
-        return rel
+        if rel != True:
+            i = 0
+            while i < int(n/2):
+                if int(s[i]) != (int(s[n -1 -i]) - 3):
+                    rel = False
+                    break
+                i += 1
+                rel = True
+    return print(rel)
 
-s = "()"
+s = "{[]}}{"
 
-print(isValid(s))
+isValid(s)
